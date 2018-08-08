bd3996c9a480c54abcc38e227a16ec33e479ebf2
Jeff L <jeffreyliu0@gmail.com>
2018-08-07 20:15:55 -0700

    added strings find longest + palindrome

--- strings/string1.c
+++ strings/string1.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-	
+	char str[] = "Hello Booboo";
+
+	strcpy(str, "Bummer");
+
+	char str1[] = "Daymz";
+
+	strcpy(str, str1);
+
+	printf("Message is: %s\n", str);
+
+	char test[100];
+
+	scanf("%[^\n]", test);
+	printf("This is it %s\n", strcat(test, str));
+
+	printf("Are they equal? %d\n", strcmp(str, test));
+
+	int i = strlen(test);
+	printf("Length of previous text: %d\n", i);
+
 	return 0;
 }
