eb970ef59bcb1ea308de6f908d031664e7c23f69
Jeff L <jeffreyliu0@gmail.com>
2018-08-12 14:03:52 -0700

    added swap function

--- pointers/pointer.c
+++ pointers/pointer.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
 
+void swap(int *a, int *b){
+
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
 int main(){
 
-	int *p;
-	float *f;
-	double *d;
-	char *k;
+	int x = 100, y = 200;
 
-	printf("%lu %lu %lu %lu\n", sizeof(p), sizeof(f), sizeof(d), sizeof(k));
-	printf("%lu %lu %lu %lu\n", sizeof(*p), sizeof(*f), sizeof(*d), sizeof(*k));
-
-	int x = 10;
-	int *y;
-	y = &x;
-	*y = 123;
-
-	printf("The address of x is: %p\n", y);
-	printf("The address of x++ is: %p\n", y + 1);
-	printf("The value of x after pointer changed: %d\n", *y);
-
-	double *a = (double *)10000;
-	double *b = (double *)2000;
-
-	printf("%d/n", p-q);
+	printf("x: %d, y: %d\n", x,y);
+	swap(&x,&y);
+	printf("x: %d, y: %d\n", x,y);
 
 	return 0;
 }
